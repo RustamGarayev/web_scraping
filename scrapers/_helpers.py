e4b6162267b539f572a2a6aefeaf85a8581e724f
RustamGarayev <rustam.garayev.std@bhos.edu.az>
2023-10-25 20:53:25 +0300

    feat: passed fetch_page test

--- scrapers/_helpers.py
+++ scrapers/_helpers.py
@@ -3,4 +3,11 @@ import typing as ty
 
 
 def fetch_page(url: str, headers: ty.Dict) -> ty.Union[requests.Response, ty.Dict[str, ty.Any]]:
-    pass
+    try:
+        return requests.get(url, headers=headers)
+    except requests.RequestException as e:
+        return {
+            "status": 500,
+            "message": f"Something went wrong\nPossible cause: {e}",
+            "body": None
+        }
